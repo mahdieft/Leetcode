7144a5cbacf3b488ce10bd3c3b6e2d4fb5fe57d0
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-03 06:54:42 -0400

    Explore - Learn - The LeetCode Beginner's Guide - Solving Your First Problem - Number of Steps to Reduce a Number to Zero

---
+++ Explore/Learn/The LeetCode Beginner's Guide/02. Challenge Problems/04. Number of Steps to Reduce a Number to Zero/number_of_steps.php
@@ -0,0 +1,15 @@
+<?php
+
+function numberOfSteps(int $num): int
+{
+    $steps = 0;
+    while ($num > 0) {
+        $steps++;
+        if ($num % 2 === 0) {
+            $num /= 2;
+        } else {
+            $num--;
+        }
+    }
+    return $steps;
+}
