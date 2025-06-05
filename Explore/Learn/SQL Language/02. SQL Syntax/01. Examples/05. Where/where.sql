121e983043e25476d1b01e805c5895eb435ab1b8
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-05 05:47:24 -0400

    Explore - Learn - SQL Language - SQL Syntax - Examples - Where

---
+++ Explore/Learn/SQL Language/02. SQL Syntax/01. Examples/05. Where/where.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM `my_schema`.`users`
+WHERE id = 1;
+
+SELECT *
+FROM `my_schema`.`users`
+WHERE id > 2;
+
+SELECT *
+FROM `my_schema`.`users`
+WHERE id <= 1;
+
+SELECT *
+FROM `my_schema`.`users`
+WHERE id != 1;
