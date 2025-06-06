49edbfd5918923bceef9b6fb4bbca1d6e74f6b07
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-06 10:44:42 -0400

    Explore - Learn - SQL Language - SQL Syntax - Examples - Like Conditions

---
+++ Explore/Learn/SQL Language/02. SQL Syntax/01. Examples/09. Like Condition/like_condition.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM `my_schema`.`users`
+WHERE name LIKE '%a%';
+
+SELECT *
+FROM `my_schema`.`users`
+WHERE name LIKE 'J%';
+
+SELECT *
+FROM `my_schema`.`users`
+WHERE name LIKE '%y';
+
+SELECT *
+FROM `my_schema`.`users`
+WHERE name LIKE 'Boo_';
